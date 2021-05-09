12af071077b8313f168cb31c09766616f4f654b4
Rushil10 <rushil.10.shah@gmail.com>
2021-05-09 20:52:40 +0530

    new

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,25 +1,53 @@
 import React from 'react';
 import './Toolbar.css'
 import {FaHome,FaUser,FaProjectDiagram,FaSuitcase,FaEnvelope} from 'react-icons/fa'
+import { useLocation } from 'react-router-dom'
 
 function Toolbar() {
+    const location = useLocation();
+    console.log(location.pathname);
     return(
         <div className="toolbar">
             <div style={{flexDirection:'column'}}>
                 <div style={{alignItems:'center',justifyContent:'center',textAlign:'center',marginBottom:25}}>
-                    <a href="/"><FaHome style={{color:'#bcdfeb',height:27,width:27}} /></a>
+                    {
+                        location.pathname === "/" ?
+                        <a href="/"><FaHome style={{color:'#fc6d6d',height:27,width:27}} /></a>
+                        :
+                        <a href="/"><FaHome style={{color:'bcdfeb',height:27,width:27}} /></a>
+                    }
                 </div>
                 <div style={{alignItems:'center',justifyContent:'center',textAlign:'center',marginBottom:25}}>
-                    <a href="/about"><FaUser style={{color:'#1da1f2',height:27,width:27}} /></a>
+                    {
+                        location.pathname === "/about" ?
+                        <a href="/about"><FaUser style={{color:'#fc6d6d',height:27,width:27}} /></a>
+                        :
+                        <a href="/about"><FaUser style={{color:'#bcdfeb',height:27,width:27}} /></a>
+                    }
                 </div>
                 <div style={{alignItems:'center',justifyContent:'center',textAlign:'center',marginBottom:25}}>
-                    <FaProjectDiagram style={{color:'#1da1f2',height:27,width:27}} />
+                    {
+                        location.pathname === "/projects" ?
+                        <a href="/projects"><FaProjectDiagram style={{color:'#fc6d6d',height:27,width:27}} /></a>
+                        :
+                        <a href="/projects"><FaProjectDiagram style={{color:'#bcdfeb',height:27,width:27}} /></a>
+                    }
                 </div>
                 <div style={{alignItems:'center',justifyContent:'center',textAlign:'center',marginBottom:25}}>
-                    <FaSuitcase style={{color:'#1da1f2',height:27,width:27}} />
+                    {
+                        location.pathname === "/experience" ?
+                        <a href="/experience"><FaSuitcase style={{color:'#fc6d6d',height:27,width:27}} /></a>
+                        :
+                        <a href="/experience"><FaSuitcase style={{color:'#bcdfeb',height:27,width:27}} /></a>
+                    }
                 </div>
                 <div style={{alignItems:'center',justifyContent:'center',textAlign:'center',marginBottom:25}}>
-                    <FaEnvelope style={{color:'#1da1f2',height:27,width:27}} />
+                    {
+                        location.pathname === "/contact" ?
+                        <a href="/contact"><FaEnvelope style={{color:'#fc6d6d',height:27,width:27}} /></a>
+                        :
+                        <a href="/contact"><FaEnvelope style={{color:'#bcdfeb',height:27,width:27}} /></a>
+                    }
                 </div>
             </div>
         </div>
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import './home.css'
-import Typical from 'react-typical'
+import './home.css';
+import Typewriter from 'typewriter-effect';
 
 function Home() {
     return (
         <div className="rest">
-            <div style={{fontSize: 55,marginLeft: 45,color:'white'}}>
+            <div style={{fontSize: 55,marginLeft: 45,color:'white',fontWeight:500}}>
                 Hi
             </div>
-            <div style={{fontSize: 55,marginLeft: 45,color:'white'}}>
-                I'm Rushil ,
+            <div style={{fontSize: 55,marginLeft: 45,color:'white',fontWeight:500}}>
+                I'm Rushil,
             </div>
-            <div style={{fontSize: 29,marginLeft: 45,color:'white'}}>
-                I'm a Developer 
+            <div style={{fontSize: 29,marginLeft: 45,color:'white',flexDirection:'row',width:79}}>
+                I'm a
+                <div style={{position:'relative',left:69,top:-38.4,color:'#fc6d6d'}}>
+                <Typewriter
+                    options={{
+                    strings: ['Student', 'Developer'],
+                    autoStart: true,
+                    loop: true,
+                }}
+                />
+                </div>
+            </div>
+            <div>
+                <button style={{height:35,backgroundColor:'transparent',borderWidth:1,borderColor:'#fc6d6d',paddingLeft:9,paddingRight:9,color:'#fc6d6d',marginLeft:45}}>Contact Me</button>
             </div>
         </div>
     )
