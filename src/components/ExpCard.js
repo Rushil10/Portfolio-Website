beca8800a1f0c4a48b4bf01958da31ec356fba45
Rushil10 <rushil.10.shah@gmail.com>
2021-05-13 12:44:27 +0530

    new

--- src/components/ExpCard.js
+++ src/components/ExpCard.js
@@ -3,31 +3,48 @@ import'./ExpCard.css';
 import mn from '../images/mn.png'
 import ps from '../images/ps.svg' 
 import apst from '../images/apst.svg'
+import n from '../images/n.png'
+import g from '../images/github.svg'
 
-function ExpCard() {
+function ExpCard(props) {
+
+    const logos = [mn,n]
+
     return(
         <div className="container">
+            <div className="dummy">
+
+        </div>
             <div className="box">
                 <div className="first-row">
                     <div className="image">
-                        <img src={mn} style={{height:41,width:41}} />
+                        <img src={logos[props.index]} style={{height:41,width:41}} />
                     </div>
                     <div className="cname">
-                        MunchIn
+                        {props.item.name}
                     </div>
                 </div>
                 <div className="position">
-                    Lead React Native Developer
+                    {props.item.role}
                 </div>
                 <div className="term">
-                    Feb 2021 - May 2021
+                    {props.item.term}
                 </div>
                 <div className="description">
-                    It was a very good experience as an intern.I got to learn many things apart from react-native like coordinating in teams, decision making and problem solving. During these 4 months i worked on every aspect of the app which helped me in increasing my skill and abilities. 
+                    {props.item.description}
                 </div>
                 <div className="links">
-                    <a href="https://play.google.com/store/apps/details?id=com.munchinapp"><img src={ps} style={{height:35,width:35,marginRight:5}} /></a>
-                    <a href=""><img src={apst} style={{height:35,width:35}} /></a>
+                    {
+                        props.item.plink!=='' &&
+                        <a href={props.item.plink}><img src={ps} style={{height:35,width:35,marginRight:5}} /></a>
+                    } {
+                        props.item.alink!=='' &&
+                        <a href=""><img src={apst} style={{height:35,width:35}} /></a>  
+                    }
+                    {
+                        props.item.glink!=='' &&
+                        <a href={props.item.glink}><img src={g} style={{height:29,width:29}} /></a>
+                    }
                 </div>
             </div>
         </div>
--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -3,12 +3,39 @@ import ExpCard from '../ExpCard';
 import './experience.css'
 
 function Experience() {
+
+    const exp = [
+        {
+            name:'MunchIn',
+            term:'Feb 2021 - May 2021',
+            role:'React Native developer',
+            description:'It was a very good experience as an intern.I got to learn many things apart from react-native like coordinating in teams, decision making and problem solving. During these 4 months i worked on every aspect of the app which helped me in increasing my skill and abilities. ',
+            plink:'https://play.google.com/store/apps/details?id=com.munchinapp',
+            alink:'',
+            glink:'',
+        },
+        {
+            name:'Gamers Prestige',
+            term:'July 2020 - Sept 2020',
+            role:'React Native Developer',
+            description:'This was my First Internship in react-native. I made a News App from scratch with search,filter and dark mode features. This helped me in understanding and getting familiar with the framework.',
+            alink:'',
+            plink:'',
+            glink:'https://github.com/Rushil10/News-App'
+        }
+    ]
+    
+    let cards = exp.map((item,index) => 
+        <ExpCard index={index} item={item} />
+    )
+
     return (
+
         <div className="main5">
             <div className="title-exp">
                 Internships
             </div>
-            <ExpCard />
+            {cards}
         </div>
     )
 }
